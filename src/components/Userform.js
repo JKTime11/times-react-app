9d82d02ac346871b954fe29f7013562980113260
sandeepnitkkr <sandeep.kumar1@timesinternet.in>
2021-07-19 12:21:13 +0530

    2 way data binding

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 
 export default function Userform() {
     const [userform, setUserform]=useState({firstname: "Sandeep"});
+    const handleEvent=function(event) {
+        setUserform({...userform, firstname:event.target.value});
+    }
     return (
         <div>
             <h3>Create User</h3>
-            <input type="text" value={userform.firstname}></input>
+            <input type="text" value={userform.firstname} onChange={handleEvent}></input>
         </div>
     )
 }
