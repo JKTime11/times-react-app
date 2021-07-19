94a3c82495abea7d405249933c55a1f086e5c0c2
sandeepnitkkr <sandeep.kumar1@timesinternet.in>
2021-07-19 14:20:40 +0530

    generic event handler

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -6,10 +6,7 @@ export default function Userform() {
         age: 20
     });
     const handleEvent=function(event) {
-        setUserform({...userform, firstname:event.target.value});
-    }
-    const handleEventForAge=function(event) {
-        setUserform({...userform, age:event.target.value});
+        setUserform({...userform, [event.target.name]:event.target.value});
     }
     const save=function(event) {
         console.log(userform);
@@ -17,8 +14,8 @@ export default function Userform() {
     return (
         <div>
             <h3>Create User</h3>
-            <input type="text" value={userform.firstname} onChange={handleEvent}></input>
-            <input type="number" value={userform.age} onChange={handleEventForAge}></input>
+            <input type="text" name="firstname" value={userform.firstname} onChange={handleEvent}></input>
+            <input type="number" name="age" value={userform.age} onChange={handleEvent}></input>
             <button onClick={save}>Save</button>
         </div>
     )
