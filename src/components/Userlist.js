322e6ab5d01d86b0e6e606d1756841212c10e48d
sandeepnitkkr <sandeep.kumar1@timesinternet.in>
2021-07-20 10:49:54 +0530

    display data dynamically using useEffect() & array.map

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,4 +1,21 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
 export default function Userlist() {
+    const [users, setUsers]=useState([]);
+
+    useEffect(function() {
+        console.log('useEffect() called...');
+        if(users.length!==0) {
+            return;
+        }
+        const promise=axios.get('http://localhost:4200/users');
+        promise.then(function(response){
+            setUsers(response.data);
+            console.log(response.data);
+        });
+    });
+
     return (
         <div>
             {/* add class table */}
@@ -10,10 +27,13 @@ export default function Userlist() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Sandeep</td>
-                        <td>20</td>
-                    </tr>
+                    {users.map(function(user, index) {
+                        return (<tr>
+                            <td>{user.firstname}</td>
+                            <td>{user.age}</td>
+                        </tr>);
+                    })}
+                    
                 </tbody>
             </table>
         </div>
